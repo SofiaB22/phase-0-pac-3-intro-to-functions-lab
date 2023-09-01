2a71ba594989a5dafbbb815d63d6fa7510567b31
+Bridgette <+sofiaeuc112002@gmail.com+>
2023-09-01 04:52:06 -0500

    Complete Lab

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  // todo
+} 
+  
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log('HELLO'.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase());
+}
+
+function sayHiToHeadphonedRomate(string) {
+console.log('hello'.toLowerCase()) 
+}
